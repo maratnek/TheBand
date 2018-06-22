7cd97bdb4a4c8b171405139d94163727e1ac5ba6
maratnek <maratn566ek@gmail.com>
2018-06-22 11:04:07 +0300

    added pause&play in player

--- player.js
+++ player.js
@@ -1,14 +1,35 @@
 $(document).ready(()=>{
-  let player = $('#audioPlayer');
+
+  // root change
+  function positionChange(trackPosition) {
+    // let trackPosition = 50;
+    $(':root').css('--track-position', `${trackPosition}%`);
+  }
+
+  var player = $('#audioPlayer').get(0);
   console.log('player', player);
+
+  function positionUpdate(player) {
+    let interv = setInterval(function () {
+      if (player && player.currentTime && player.duration)
+      {
+        console.log(player.currentTime, player.duration);
+        positionChange(player.currentTime/player.duration*100);
+      }
+    }, 10);
+  }
+  positionUpdate(player);
+
   $('.control').on('click',(ev)=>{
     let btnName = ev.target.className;
+    let time = 200;
     if (btnName === 'play')
     {
-      let time = 200;
+    player.play();
       $('.control .play').hide(time);
       $('.control .pause').show(time);
     } else {
+    player.pause();
       $('.control .play').show(time);
       $('.control .pause').hide(time);
     }
